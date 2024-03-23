0f7c12e65c8eb8d49320034efc49e1fb0a9e5b95
metavind <metavind@gmail.com>
2024-03-23 21:11:57 +0100

    feat: add interfaces for inference models and frameworks

---
+++ src/models/index.ts
@@ -0,0 +1,16 @@
+export interface InferenceModel {
+  id: string;
+  name: string;
+  description: string;
+  thumbnail: string;
+  compatibleFrameworks: string[];
+  inputShape: number[];
+  outputShape: number[];
+  // run(input: number[]): number[];
+}
+
+export interface Framework {
+  id: string;
+  name: string;
+  description: string;
+}
