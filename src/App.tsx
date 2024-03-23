0dd345f663b48433a0d13de1f24f9e579253f270
metavind <metavind@gmail.com>
2024-03-23 21:08:38 +0100

    style: change title and heading of page to ZK Nets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">ZK Nets</h1>
+    </div>
+  );
 }
 
 export default App;
