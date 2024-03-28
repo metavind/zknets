bfe383ee7a8c7d82c1c119d08910a681101b989c
metavind <metavind@gmail.com>
2024-03-28 11:12:35 +0100

    feat: add onnxruntime-web package and add fix for copying wasm files to make it work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,17 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/onnxruntime-web/dist/*.wasm',
+          dest: 'dist',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
