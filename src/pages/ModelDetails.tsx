991c7d4030567f51667b8d586470279826ada276
metavind <metavind@gmail.com>
2024-03-29 17:47:12 +0100

    feat: improve layout and ensure states are reset when new point is selected

--- src/pages/ModelDetails.tsx
+++ src/pages/ModelDetails.tsx
@@ -25,6 +25,10 @@ const ModelDetails: React.FC<ModelDetailsProps> = ({
   const [noirInstance, setNoirInstance] = useState<Noir | undefined>(undefined);
 
   const handleInputChange = (newInput: number[]) => {
+    setInferenceOutput(null);
+    setZkOutput(null);
+    setProofData(undefined);
+    setProofHex(null);
     setInput(newInput);
   };
 
@@ -84,7 +88,7 @@ const ModelDetails: React.FC<ModelDetailsProps> = ({
         </div>
         <p className="mt-2">Selected Input: {input.join(', ')}</p>
       </div>
-      <div className="mb-6 flex justify-center space-x-4">
+      <div className="mb-8 flex justify-center space-x-4">
         <button
           className="mb-2 me-2 w-60 rounded-lg border border-gray-800 bg-gray-100 px-10 py-5 text-center text-lg font-semibold text-gray-700 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
           type="button"
@@ -113,24 +117,37 @@ const ModelDetails: React.FC<ModelDetailsProps> = ({
           (on-chain)
         </button>
       </div>
-      <div className="mb-6">
-        {inferenceOutput && proofData && (
-          <div>
-            <h4>Inference Result:</h4>
-            <p>{inferenceOutput.join(', ')}</p>
-            <h4>Output:</h4>
-            <p>{zkOutput?.join(', ')}</p>
-            <h4>Generated Proof:</h4>
-            <p>{proofHex}</p>
+      <div className="mb-6 flex justify-center">
+        <div className="w-3/4 border-t-2 border-gray-400 pt-6">
+          <div className="px-40">
+            <div className="mb-6 mt-2 flex justify-center space-x-32">
+              <div>
+                <h4 className="mb-2 text-xl font-semibold">Expected Output</h4>
+                <p className="text-center">
+                  {inferenceOutput ? inferenceOutput.join(', ') : '-'}
+                </p>
+              </div>
+              <div>
+                <h4 className="mb-2 text-xl font-semibold">Model Output</h4>
+                <p className="text-center">
+                  {zkOutput ? zkOutput.join(', ') : '-'}
+                </p>
+              </div>
+            </div>
           </div>
-        )}
-      </div>
-      <div className="mb-6">
-        {proofData && (
-          <div>
-            <h4>Verified Proof:</h4>
+          <div className="mb-6">
+            {inferenceOutput && proofData && (
+              <div>
+                <h4 className="mb-2 text-xl font-semibold">Proof</h4>
+                <div className="h-40 overflow-y-auto rounded-lg border border-gray-300 bg-gray-50 p-4">
+                  <p className="whitespace-pre-wrap font-mono text-sm">
+                    {proofHex}
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
