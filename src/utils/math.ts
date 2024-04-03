8468b62c1c57b64b1d7e2201e59e7eef47645e82
metavind <metavind@gmail.com>
2024-04-03 13:25:43 +0200

    feat: add functions to prepare input for inference and circuit

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,3 +1,5 @@
+import { mapToPrimeField } from '@/utils/proof';
+
 export const gaussianRandom = () => {
   let u = 0;
   let v = 0;
@@ -5,3 +7,13 @@ export const gaussianRandom = () => {
   while (v === 0) v = Math.random();
   return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
 };
+
+export const prepareInputForInference = (
+  input: number[],
+  scalingFactor: number
+): number[] => input.map((val) => Math.round(val * scalingFactor));
+
+export const prepareInputForCircuit = (
+  input: number[],
+  scalingFactor: number
+): bigint[] => mapToPrimeField(prepareInputForInference(input, scalingFactor));
